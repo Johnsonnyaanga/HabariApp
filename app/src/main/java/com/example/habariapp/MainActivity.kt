fc65f3fb8daa956407892dba35059830b3db9dd0
unknown <johnsonyaanga@gmail.com>
2021-06-16 16:55:11 +0300

    featureNavigationDrawer

--- app/src/main/java/com/example/habariapp/MainActivity.kt
+++ app/src/main/java/com/example/habariapp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.habariapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.habariapp.database.NewsDatabase
 import com.example.habariapp.repository.NewsRepository
@@ -13,13 +19,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: NewsViewModel
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_nav.setupWithNavController(nav_host_fragment_cont.findNavController())
         val newsRepository = NewsRepository(NewsDatabase(this))
         val newsViewModelFactoryProvider = NewsViewModelFactoryProvider(application, newsRepository)
-        viewModel = ViewModelProvider(this,newsViewModelFactoryProvider).get(NewsViewModel::class.java)
+        viewModel = ViewModelProvider(this,newsViewModelFactoryProvider)
+            .get(NewsViewModel::class.java)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navController = findNavController(R.id.nav_host_fragment_cont)
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        navigation_view.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                ||super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/habariapp/util/InternetCheck.kt
@@ -0,0 +1,47 @@
+package com.example.habariapp.util
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
+import com.example.habariapp.HabariApplication
+
+class InternetCheck(application: Application) :AndroidViewModel(application) {
+    fun hasInternetConnection():Boolean{
+        val conectivityManager = getApplication<HabariApplication>().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            val activeNetWork = conectivityManager.activeNetwork ?: return false
+            val capabilities =  conectivityManager.getNetworkCapabilities(activeNetWork) ?: return false
+
+            return when{
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+
+
+
+        }else{
+
+            conectivityManager.activeNetworkInfo?.run {
+                return when(type){
+                    ConnectivityManager.TYPE_WIFI ->  true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+
+        }
+
+
+
+
+        return false
+    }
+}
