15f94eb51781e7f165b529affc49aaf8fc00b4f3
JOHNSON NYAANGA <32130274+Johnsonnyaanga@users.noreply.github.com>
2021-08-12 21:34:19 +0300

    Update Constants.kt

--- app/src/main/java/com/example/habariapp/util/Constants.kt
+++ app/src/main/java/com/example/habariapp/util/Constants.kt
@@ -2,8 +2,7 @@ package com.androiddevs.NewsApp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "0b662c1d6cfc4319982fe587169781e6"
-        //const val API_KEY = "acdc263e18884290abb6060b0c4a1633"
+        const val API_KEY = "get your own api key from newsapi.org"
         const val BASE_URL="https://newsapi.org/"
         const val SEARCH_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
